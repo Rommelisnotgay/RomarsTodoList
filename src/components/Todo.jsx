ad2b2c6929c4cace235d51a1cc0735c0624fdf2d
Rommel Tarek <134862210+Rommelisnotgay@users.noreply.github.com>
2025-11-29 20:17:35 +0200

    v2

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -71,17 +71,18 @@ const Todo = () => {
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
           ref={inputRef}
-          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
+          className="bg-transparent border-0 outline-none h-14 pl-6 pr-2 placeholder:text-slate-600 flex-none w-[calc(100%-110px)] max-[350px]:w-[calc(100%-100px)]"
           type="text"
           placeholder="Add Your Task"
         />
         <button
           onClick={add}
-          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer transition-all duration-200 ease-in hover:bg-amber-700 max-[350px]:text-[9px]"
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer transition-all duration-200 ease-in hover:bg-amber-700 "
         >
           ADD +
         </button>
       </div>
+
       <div>
         <button
           className="flex items-center justify-start w-fit
